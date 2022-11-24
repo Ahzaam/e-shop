371cd242958fb67e78d98a18b452aab7334e8ab1
Fawmee Ahzam <93484273+dev001-silverlineit@users.noreply.github.com>
2022-11-25 01:15:39 +0530

    team

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { AuthenticateComponent } from './Components/authenticate/authenticate.component';
 import { HomeComponent } from './Components/home/home.component';
 import { ShopComponent } from './Components/shop/shop.component';
+import { TeamComponent } from './Components/team/team.component';
 import { UserProfileComponent } from './Components/user-profile/user-profile.component';
 
 const routes: Routes = [
@@ -10,6 +11,7 @@ const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'auth', component: AuthenticateComponent },
   { path: 'profile', component: UserProfileComponent },
+  { path: 'team', component: TeamComponent },
 ];
 
 @NgModule({
