cb03096ca81dd76d5848f3012b7f012bf0546a27
Ahzaam <93484273+dev001-silverlineit@users.noreply.github.com>
2022-12-03 01:15:13 +0530

    home page

--- src/app/Components/products-carousel/products-carousel.component.ts
+++ src/app/Components/products-carousel/products-carousel.component.ts
@@ -7,18 +7,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductsCarouselComponent implements OnInit {
   defaultTransform = 0;
+  width: number | any;
   slider: HTMLDivElement | any;
   constructor() {}
 
   ngOnInit(): void {
     this.slider = document.getElementById('slider');
-    setInterval(() => {
-      this.goNext(this.slider);
-    }, 3000);
+  }
+  ngAfterViewInit() {
+    let rectw: number = document
+      .getElementById('slider-w-0')
+      ?.getBoundingClientRect().width as number;
+    this.width = rectw + rectw / 8;
   }
 
   goNext(slider: any) {
-    this.defaultTransform = this.defaultTransform - 398;
+    this.defaultTransform =
+      this.defaultTransform - (this.width + this.width / 8);
 
     if (Math.abs(this.defaultTransform) >= slider.scrollWidth / 1.7)
       this.defaultTransform = 0;
@@ -27,7 +32,9 @@ export class ProductsCarouselComponent implements OnInit {
 
   goPrev(slider: any) {
     if (Math.abs(this.defaultTransform) === 0) this.defaultTransform = 0;
-    else this.defaultTransform = this.defaultTransform + 398;
+    else
+      this.defaultTransform =
+        this.defaultTransform + (this.width + this.width / 8);
     slider.style.transform = 'translateX(' + this.defaultTransform + 'px)';
   }
 }
