c2a6030ba44d501c7e02f1d294e4fc8a339f2670
Fawmee Ahzam <93484273+dev001-silverlineit@users.noreply.github.com>
2023-02-21 14:58:42 +0530

    commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,24 +1,24 @@
 export const environment = {
-  // firebase: {
-  //   apiKey: 'AIzaSyBTMseh43erP9cuziLu5WYnusF8G97TE8w',
-  //   authDomain: 'online-shoppings.firebaseapp.com',
-  //   projectId: 'online-shoppings',
-  //   storageBucket: 'online-shoppings.appspot.com',
-  //   messagingSenderId: '819338683493',
-  //   appId: '1:819338683493:web:62e489aabd8d46193d128c',
-  //   measurementId: 'G-K16PHK8K2H',
-  // },
-
   firebase: {
-    projectId: 'e-shop-1626e',
-    appId: '1:37641869664:web:1c54209eeea2d30cf56605',
-    databaseURL: 'https://e-shop-1626e-default-rtdb.firebaseio.com',
-    storageBucket: 'e-shop-1626e.appspot.com',
-    locationId: 'us-central',
-    apiKey: 'AIzaSyB36Zv6KE2ch_gQekLmYOUOQD6b0ohImUA',
-    authDomain: 'e-shop-1626e.firebaseapp.com',
-    messagingSenderId: '37641869664',
-    measurementId: 'G-PD12EMP5ZD',
+    apiKey: 'AIzaSyBTMseh43erP9cuziLu5WYnusF8G97TE8w',
+    authDomain: 'online-shoppings.firebaseapp.com',
+    projectId: 'online-shoppings',
+    storageBucket: 'online-shoppings.appspot.com',
+    messagingSenderId: '819338683493',
+    appId: '1:819338683493:web:62e489aabd8d46193d128c',
+    measurementId: 'G-K16PHK8K2H',
   },
+
+  // firebase: {
+  //   projectId: 'e-shop-1626e',
+  //   appId: '1:37641869664:web:1c54209eeea2d30cf56605',
+  //   databaseURL: 'https://e-shop-1626e-default-rtdb.firebaseio.com',
+  //   storageBucket: 'e-shop-1626e.appspot.com',
+  //   locationId: 'us-central',
+  //   apiKey: 'AIzaSyB36Zv6KE2ch_gQekLmYOUOQD6b0ohImUA',
+  //   authDomain: 'e-shop-1626e.firebaseapp.com',
+  //   messagingSenderId: '37641869664',
+  //   measurementId: 'G-PD12EMP5ZD',
+  // },
   production: true,
 };
